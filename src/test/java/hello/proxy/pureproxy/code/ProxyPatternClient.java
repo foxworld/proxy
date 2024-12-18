892c65e14c10327a6b721fe646cd48d45634d948
이훈구 <foxworld@gmail.com>
2024-12-18 09:07:32 +0900

    ProxyPattern 테스트 적용 * 접근제어

---
+++ src/test/java/hello/proxy/pureproxy/code/ProxyPatternClient.java
@@ -0,0 +1,13 @@
+package hello.proxy.pureproxy.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ProxyPatternClient {
+
+    private final Subject subject;
+
+    public void execute() {
+        subject.operation();
+    }
+}
