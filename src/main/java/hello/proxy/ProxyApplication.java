1dc3af903a54f5988994dd77ca3c0acfc0a8e419
이훈구 <foxworld@gmail.com>
2025-01-13 15:15:51 +0900

    프록시 팩토리 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV1;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class}) // 원하는 컨피그를 정의하기 위해 설정
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class}) // 인터페이스를 이용한 proxy / 구체클래스를 로  proxy 구현
 //@Import(DynamicProxyBasicConfig.class) // 동적프로시를 이용하여 남기는 config
-@Import(DynamicProxyFilterConfig.class) // 특정패턴만 로그를 남기는 Config
+//@Import(DynamicProxyFilterConfig.class) // 특정패턴만 로그를 남기는 Config
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // ComponentScan 위치를 지정하여 필요한부분만 스캔
 public class ProxyApplication {
 
