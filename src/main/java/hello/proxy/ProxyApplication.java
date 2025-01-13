88273957b178bdd6115dd444511fb2529a27b5a6
이훈구 <foxworld@gmail.com>
2025-01-13 15:36:55 +0900

    프록시 팩토리 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +13,8 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class}) // 인터페이스를 이용한 proxy / 구체클래스를 로  proxy 구현
 //@Import(DynamicProxyBasicConfig.class) // 동적프로시를 이용하여 남기는 config
 //@Import(DynamicProxyFilterConfig.class) // 특정패턴만 로그를 남기는 Config
-@Import(ProxyFactoryConfigV1.class) // proxyfactory 적용하는 config
+//@Import(ProxyFactoryConfigV1.class) // ProxyFactory interface 적용하는 config
+@Import(ProxyFactoryConfigV2.class) // ProxyFactory 구체클레스 cglib 적용하는 config
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // ComponentScan 위치를 지정하여 필요한부분만 스캔
 public class ProxyApplication {
 
